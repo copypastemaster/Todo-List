d40db20e5f5f604faff0f2c51e2516f0a221225a
Koki <enoch.binas@gmail.com>
2023-04-20 20:40:07 +0800

    initial styles added

--- src/todosClass.js
+++ src/todosClass.js
@@ -1,6 +1,13 @@
-class TodoProjects {
-  constructor(title, remove) {
-    this.title = title;
+export default class TodoProjects {
+  constructor(title_s) {
+    this.title_s = title_s;
+  }
+}
+
+class TodoItems {
+  constructor(todo, dueDate, remove) {
+    this.todo = todo;
+    this.dueDate = dueDate;
     this.remove = remove;
   }
 }
